0cb16e060480ac64bc2b58d6319600e58e65c280
jeffgertler <jeffgertler@gmail.com>
2018-04-13 12:31:42 -0400

    bloch point sweep added

---
+++ bloch_points.bat
@@ -0,0 +1,12 @@
+@echo off
+
+python better_ss.py   0     0   -2j     1   .2
+python better_ss.py   .5    0   -2j     1   .2
+python better_ss.py   .5    .5  -2j     1   .2
+python better_ss.py   .5    1   -2j     1   .2
+python better_ss.py   .5    1.5 -2j     1   .2
+python better_ss.py   1     0   -2j     1   .2
+
+
+
+pause
